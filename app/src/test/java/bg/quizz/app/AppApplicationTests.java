1d274044d8eb943e85bb441714786999a2f7f79e
OrlinGit <orlin.nikolov@hotmail.com>
2025-10-26 11:38:03 +0200

    Initial commit

---
+++ app/src/test/java/bg/quizz/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.quizz.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
