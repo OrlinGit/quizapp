b2fbdaed1c43286cfcdb86cc1f888cc4d4250e89
OrlinGit <orlin.nikolov@hotmail.com>
2025-10-26 12:16:45 +0200

    "Question class created"

---
+++ app/src/main/java/bg/quizz/app/models/Question.java
@@ -0,0 +1,50 @@
+package bg.quizz.app.models;
+
+import java.util.List;
+
+public class Question {
+
+    public int id;
+    public String questionText;
+    public List<String> options;
+    public String correctAnswer;
+
+    public Question(int id, String questionText, List<String> options, String correctAnswer) {
+        this.id = id;
+        this.questionText = questionText;
+        this.options = options;
+        this.correctAnswer = correctAnswer;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getQuestionText() {
+        return questionText;
+    }
+
+    public void setQuestionText(String questionText) {
+        this.questionText = questionText;
+    }
+
+    public List<String> getOptions() {
+        return options;
+    }
+
+    public void setOptions(List<String> options) {
+        this.options = options;
+    }
+
+    public String getCorrectAnswer() {
+        return correctAnswer;
+    }
+
+    public void setCorrectAnswer(String correctAnswer) {
+        this.correctAnswer = correctAnswer;
+    }
+}
